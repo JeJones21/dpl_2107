b58f48364e250ac36176508ba9d85f0b4a57bfe3
Jessica Jones <daddehjones@gmail.com>
2021-07-29 16:53:38 -0400

    Add more attributes to library object.

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe Library do
   describe 'Object Methods' do
     it "has readable attributes" do
       expect(@dpl.name).to eq("Denver Public Library")
+      expect(@dpl.books).to eq([])
+      expect(@dpl.authors).to eq([])
     end
   end
 end
