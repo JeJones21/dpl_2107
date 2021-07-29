7e1c881c54c07ab767d381c3b8708287e732f999
Jessica Jones <daddehjones@gmail.com>
2021-07-29 15:41:20 -0400

    Create Author instance.

---
+++ spec/author_spec.rb
@@ -0,0 +1,12 @@
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do
+  before :each do
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
+  describe 'Object Existence' do
+    expect(@charlotte_bronte).to be_a(Author)
+  end
+end
