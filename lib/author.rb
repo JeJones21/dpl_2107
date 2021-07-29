88f90a8e56ddec00898998793a184224229d0bf1
Jessica Jones <daddehjones@gmail.com>
2021-07-29 16:41:20 -0400

    Create gnarly write method 1 hour later.

--- lib/author.rb
+++ lib/author.rb
@@ -2,25 +2,44 @@ class Author
 
   attr_reader :first_name,
               :last_name,
-              :books
+              :books,
+              :title
 
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
     @books = []
+    # @title = title
   end
 
   def name
     @first_name + " " + @last_name
   end
 
-  def write(book, publication_date)
+  def add_book(book)
+    @books << book
+  end
+
+  def write(title, publication_date)
     #I need to create a book with the book class info...
     #I also need to add the book...
-
+    #let's break this bad boy down...
+    #A book was a hash in the book class...
+    #I don't know if this route was conventional but I think it is working...
+    #I guess I could initialize a book...
+    #Ignore my notes, I need these for studying.
+    #Instance of book use to be_a
+    #Book object use to eq
+    write_book = {
+      "author_first_name": @first_name,
+      "author_last_name": @last_name,
+      "title": title,
+      "publication_date": publication_date
+    }
+      book = Book.new(write_book)
+      add_book(book)
+      return book
   end
 
-  def add_book(book)
-    @books << book 
-  end
+
 end
