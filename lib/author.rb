25f3eae5fa0f5f37bc4bc60b9697a989c0ec5a8d
Jessica Jones <daddehjones@gmail.com>
2021-07-29 15:52:42 -0400

    Add empty books array to initialize and attr_reader.

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,13 @@
 class Author
 
   attr_reader :first_name,
-              :last_name
+              :last_name,
+              :books 
 
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
+    @books = []
   end
 
   def name
