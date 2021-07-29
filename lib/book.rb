643ffaf58ca88901d0e64af3541a78de65117001
Jessica Jones <daddehjones@gmail.com>
2021-07-29 15:11:34 -0400

    Create attr_reader.

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,10 @@
 class Book
 
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+
   def initialize(info)
     @author_first_name = info[:author_first_name]
     @author_last_name = info[:author_first_name]
